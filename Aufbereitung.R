625932835bc938e130cd8cc3fa094f4023d6f212
HannesMK <hanf007@hotmail.de>
2019-12-02 12:08:45 +0100

    Ordnen der Wochentage; minimale Anpassungen in "Visualisierung.Rmd"

--- Aufbereitung.R
+++ Aufbereitung.R
@@ -20,20 +20,20 @@ setwd(proj_pfad)
 # Pfad zu den Daten erstellen
 daten_pfad <- file.path(proj_pfad, "data")
 
-# Angabe aller Namen von Dateien, die verknuepft werden sollen (müssen alle eine Spalte namens "Datum" haben und das Komma als
+# Angabe aller Namen von Dateien, die verknuepft werden sollen (muessen alle eine Spalte namens "Datum" haben und das Komma als
 # Trennzeichen haben)
 dateinamen <- c("umsatzdaten_gekuerzt.csv", "kiwo.csv", "wetter.csv", "monatsdaten.csv")
 
 
-# Zusammenführung der Daten
+# Zusammenfuehrung der Daten
 
-# Abspeichern des ersten Datensatzes, zu dem die weiteren in der folgenden Schleife hinzugefügt werden
+# Abspeichern des ersten Datensatzes, zu dem die weiteren in der folgenden Schleife hinzugefuegt werden
 daten <- read.csv(file.path(daten_pfad, dateinamen[1]))
 
 # Formatieren der Spalte "Datum" als Datentyp "Date"
 daten$Datum <- as.Date(daten$Datum)
 
-# Hinzufügen der weiteren Datensaetze
+# Hinzufuegen der weiteren Datensaetze
 for (j in 2:length(dateinamen)) {
   # Einlesen des aktuellen Datensatzes und Abspeichern in einem temporaeren Objekt
   tmp_daten <- read.csv(file.path(daten_pfad, dateinamen[j]))
@@ -41,7 +41,7 @@ for (j in 2:length(dateinamen)) {
   # Formatieren der Spalte "Datum" als Datentyp "Date"
   tmp_daten$Datum <- as.Date(tmp_daten$Datum)
   
-  # Zusammenführen der Daten auf Grundlage der Spalte "Datum"
+  # Zusammenfuehren der Daten auf Grundlage der Spalte "Datum"
   daten <- left_join(daten, tmp_daten, by = "Datum")
 }
 
@@ -49,17 +49,17 @@ for (j in 2:length(dateinamen)) {
 # Aufbereitung der Daten
 
 # Sinnvolle Bezeichnung der unterschiedlichen Warengruppen
-daten$Warengruppe <- factor(daten$Warengruppe, levels = 1:6, labels = c("Brot", "Brötchen", "Croissant", "Konditorei", "Kuchen", "Saisonbrot"))
+daten$Warengruppe <- factor(daten$Warengruppe, levels = 1:6, labels = c("Brot", "Broetchen", "Croissant", "Konditorei", "Kuchen", "Saisonbrot"))
 
-# NA-Werte fuer die Kieler Woche als 0 kodieren (im Datensatz sind nur die Daten für die Kieler Woche enthalten, die
+# NA-Werte fuer die Kieler Woche als 0 kodieren (im Datensatz sind nur die Daten fuer die Kieler Woche enthalten, die
 # restlichen fehlen)
 daten$KielerWoche[is.na(daten$KielerWoche)] <- 0
 
 # Umwandeln des Kieler-Woche-Vektors in einen Faktor
 daten$KielerWoche <- factor(daten$KielerWoche, levels = c(0, 1), labels = c("Keine Kieler Woche", "Kieler Woche"))
 
-# Hinzufügen einer Spalte, die die Wochentage beinhaltet
-daten$Wochentag <- weekdays(daten$Datum)
+# Hinzufuegen einer Spalte, die die Wochentage (geordnet) beinhaltet
+daten$Wochentag <- ordered(weekdays(daten$Datum), levels = c("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"))
 
 # Ordnen des Faktors zur Zeit im Monat
 daten$Zeit_Im_Monat <- ordered(daten$Zeit_Im_Monat, levels = c("Monatsanfang", "Monatsmitte", "Monatsende"))
