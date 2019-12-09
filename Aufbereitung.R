a78fd289ed3d63c761cb8a321bfe5c6cc40c41c7
HannesMK <hanf007@hotmail.de>
2019-12-09 20:30:27 +0100

    Jahreszeiten geordnet und den Umsatz für die Jahreszeiten visualisiert. Umsatz für einige Wetterdaten visualisiert. "Visualisierung.Rmd" angepasst, sodass auch Umlaute gehandelt werden können.

--- Aufbereitung.R
+++ Aufbereitung.R
@@ -1,4 +1,4 @@
-# Installieren der benoetigten Pakete
+# Installieren der benötigten Pakete
 if (!require(rstudioapi)) {
   install.packages("rstudioapi")
   library(rstudioapi) 
@@ -20,28 +20,28 @@ setwd(proj_pfad)
 # Pfad zu den Daten erstellen
 daten_pfad <- file.path(proj_pfad, "data")
 
-# Angabe aller Namen von Dateien, die verknuepft werden sollen (muessen alle eine Spalte namens "Datum" haben und das Komma als
+# Angabe aller Namen von Dateien, die verknüpft werden sollen (müssen alle eine Spalte namens "Datum" haben und das Komma als
 # Trennzeichen haben)
 dateinamen <- c("umsatzdaten_gekuerzt.csv", "kiwo.csv", "wetter.csv", "monatsdaten.csv", "jahresdaten.csv")
 
 
-# Zusammenfuehrung der Daten
+# Zusammenführung der Daten
 
-# Abspeichern des ersten Datensatzes, zu dem die weiteren in der folgenden Schleife hinzugefuegt werden
+# Abspeichern des ersten Datensatzes, zu dem die weiteren in der folgenden Schleife hinzugefügt werden
 daten <- read.csv(file.path(daten_pfad, dateinamen[1]))
 
 # Formatieren der Spalte "Datum" als Datentyp "Date"
 daten$Datum <- as.Date(daten$Datum)
 
-# Hinzufuegen der weiteren Datensaetze
+# Hinzufügen der weiteren Datensätze
 for (j in 2:length(dateinamen)) {
-  # Einlesen des aktuellen Datensatzes und Abspeichern in einem temporaeren Objekt
+  # Einlesen des aktuellen Datensatzes und Abspeichern in einem temporären Objekt
   tmp_daten <- read.csv(file.path(daten_pfad, dateinamen[j]))
   
   # Formatieren der Spalte "Datum" als Datentyp "Date"
   tmp_daten$Datum <- as.Date(tmp_daten$Datum)
   
-  # Zusammenfuehren der Daten auf Grundlage der Spalte "Datum"
+  # Zusammenführen der Daten auf Grundlage der Spalte "Datum"
   daten <- left_join(daten, tmp_daten, by = "Datum")
 }
 
@@ -49,18 +49,20 @@ for (j in 2:length(dateinamen)) {
 # Aufbereitung der Daten
 
 # Sinnvolle Bezeichnung der unterschiedlichen Warengruppen
-daten$Warengruppe <- factor(daten$Warengruppe, levels = 1:6, labels = c("Brot", "Broetchen", "Croissant", "Konditorei", "Kuchen", "Saisonbrot"))
+daten$Warengruppe <- factor(daten$Warengruppe, levels = 1:6, labels = c("Brot", "Brötchen", "Croissant", "Konditorei", "Kuchen", "Saisonbrot"))
 
-# NA-Werte fuer die Kieler Woche als 0 kodieren (im Datensatz sind nur die Daten fuer die Kieler Woche enthalten, die
+# NA-Werte für die Kieler Woche als 0 kodieren (im Datensatz sind nur die Daten für die Kieler Woche enthalten, die
 # restlichen fehlen)
 daten$KielerWoche[is.na(daten$KielerWoche)] <- 0
 
 # Umwandeln des Kieler-Woche-Vektors in einen Faktor
 daten$KielerWoche <- factor(daten$KielerWoche, levels = c(0, 1), labels = c("Keine Kieler Woche", "Kieler Woche"))
 
-# Hinzufuegen einer Spalte, die die Wochentage (geordnet) beinhaltet
+# Hinzufügen einer Spalte, die die Wochentage (geordnet) beinhaltet
 daten$Wochentag <- ordered(weekdays(daten$Datum), levels = c("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"))
 
 # Ordnen des Faktors zur Zeit im Monat
 daten$Zeit_Im_Monat <- ordered(daten$Zeit_Im_Monat, levels = c("Monatsanfang", "Monatsmitte", "Monatsende"))
 
+# Ordnen des Faktors zu den Jahreszeiten
+daten$Jahrzeiten <- ordered(daten$Jahrzeiten, levels = c("Frühling", "Sommer", "Herbst", "Winter"))
